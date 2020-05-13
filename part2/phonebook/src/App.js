73c107ec5bd6793fbed2354036235bb0d27f4cf9
Caio Souza <caio.souza@aevo.com.br>
2020-05-13 20:55:39 -0300

    Exercises 2.6.-2.10.

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,89 @@
+import React, { useState } from 'react'
+
+const Filter = ({ value, onChange }) => (
+  <form onSubmit={e => e.preventDefault()}>
+    <div>filter shown with: <input value={value} onChange={e => onChange(e.target.value)} /></div>
+  </form>
+);
+
+const PersonForm = ({ onSubmit, name, onChangeName, number, onChangeNumber }) => (
+  <form onSubmit={onSubmit}>
+    <div>name: <input value={name} onChange={e => onChangeName(e.target.value)} /></div>
+    <div>number: <input value={number} onChange={e => onChangeNumber(e.target.value)} /></div>
+    <div><button type="submit">add</button></div>
+  </form>
+)
+
+const Persons = ({ filter, persons }) => {
+  const personsFiltered = persons.filter(p =>
+    p.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
+  return (
+    <table>
+      <tbody>
+        {personsFiltered.map(person =>
+          <tr key={person.name}>
+            <td>{person.name}</td>
+            <td>{person.number}</td>
+          </tr>
+        )}
+      </tbody>
+    </table>
+  )
+}
+
+const App = () => {
+  // Initial state
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ]);
+
+  const [filter, setFilter] = useState('');
+  const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
+
+  // Add a new phone number to the list
+  const addPhone = event => {
+    event.preventDefault();
+
+    if (persons.find(({ name }) => name === newName)) {
+      alert(`${newName} is already added to phonebook`)
+      return;
+    }
+
+    const person = {
+      name: newName,
+      number: newNumber
+    };
+
+    setPersons(persons.concat(person));
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+
+      <Filter value={filter} onChange={setFilter} />
+
+      <h3>add a new</h3>
+
+      <PersonForm
+        onSubmit={addPhone}
+        name={newName}
+        onChangeName={setNewName}
+        number={newNumber}
+        onChangeNumber={setNewNumber}
+      />
+
+      <h3>Numbers</h3>
+
+      <Persons filter={filter} persons={persons} />
+    </div>
+  )
+}
+
+export default App

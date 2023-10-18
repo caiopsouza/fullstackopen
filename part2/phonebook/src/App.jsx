7f2c89bb494f91968fc521695467e24e0cfaf73f
Caio Souza <caiophilipevs@gmail.com>
2023-10-18 12:27:42 -0300

    Exercise 2.11: The Phonebook Step6

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import {useState} from 'react'
+import {useEffect, useState} from 'react'
+import axios from "axios";
 
 const Filter = ({nameFilter, setNameFilter}) => {
     return (<>
@@ -49,12 +50,7 @@ const Numbers = ({filter, persons}) => {
 }
 
 const App = () => {
-    const [persons, setPersons] = useState([
-        {name: 'Arto Hellas', number: '040-123456', id: 1},
-        {name: 'Ada Lovelace', number: '39-44-5323523', id: 2},
-        {name: 'Dan Abramov', number: '12-43-234345', id: 3},
-        {name: 'Mary Poppendieck', number: '39-23-6423122', id: 4}
-    ]);
+    const [persons, setPersons] = useState([]);
     const [newName, setNewName] = useState('');
     const [newNumber, setNewNumber] = useState('');
     const [nameFilter, setNameFilter] = useState('');
@@ -73,6 +69,12 @@ const App = () => {
         setNewName('');
         setNewNumber('');
     }
+
+    useEffect(() => {
+        axios
+            .get('http://localhost:3001/persons')
+            .then(response => setPersons(response.data))
+    }, []);
 
     return (<>
         <h2>Phonebook</h2>
